4d96400adedd742c48578310375d760472fd0fb6
ChoopickMrr <76112707+ChoopickMrr@users.noreply.github.com>
2020-12-16 17:21:07 +0300

    Create Laba1.cpp

---
+++ Laba1.cpp
@@ -0,0 +1,16 @@
+
+#include <stdafx.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(0, "");
+	int a,b;
+	cin >> a;
+	cin >> b;
+	int c = a + b;
+		cout << c << endl;
+		system("pause");
+    return 0;
+}
+
